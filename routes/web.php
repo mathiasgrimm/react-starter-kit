e57f4f038e2a92ee9fc20d3f50fcc627eda4b02c
Mathias Grimm <mathiasgrimm@gmail.com>
2025-06-03 10:19:18 -0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,12 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+Route::get('/test1', function () {
+    return [
+        'ip' => request()->ip(),
+        'ips' => request()->ips(),
+    ];
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
