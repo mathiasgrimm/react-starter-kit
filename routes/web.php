9012fe744b7d373c0b6b4593983d86a41b5022b8
Mathias Grimm <mathiasgrimm@gmail.com>
2025-06-04 16:30:03 -0300

    wip

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/test1', function () {
     return [
         'ip' => request()->ip(),
         'ips' => request()->ips(),
+        'headers' => request()->headers->all(),
     ];
 });
 
